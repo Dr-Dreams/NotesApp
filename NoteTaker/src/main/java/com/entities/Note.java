e8103c47fc0ef5e88a9b25313d6f2d9a89481b65
Mausam Kumar <82662657+SpiderMurphy69@users.noreply.github.com>
2021-11-18 01:37:17 +0530

    Add files via upload

---
+++ NoteTaker/src/main/java/com/entities/Note.java
@@ -0,0 +1,66 @@
+package com.entities;
+
+import java.util.Date;
+import java.util.Random;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "notes")
+public class Note {
+	@Id
+	private int id;
+	private String title;
+	@Column(length = 1500)
+	private StringBuffer content;
+	private Date addedDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public StringBuffer getContent() {
+		return content;
+	}
+
+	public void setContent(StringBuffer content) {
+		this.content = content;
+	}
+
+	public Date getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	public Note(String title, StringBuffer content, Date addedDate) {
+		super();
+		this.id = new Random().nextInt(100000);
+		this.title = title;
+		this.content = content;
+		this.addedDate = addedDate;
+	}
+
+	public Note() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
